92f049d8a35bd1cfed0036f28e941fc05d59574b
guo <guoyaoquan@wy.net>
2017-02-09 17:26:44 +0800

    finish task20

--- task21/task.js
+++ task21/task.js
@@ -1,77 +1,47 @@
-// $(function () {
-
-//     var list = $('#list');
-//     var input_list = $('#input_list');
-//     list.on("click", "td", function () {
-//         this.remove();
-//     });
-
-//     $('#left_input').on('click', function () {
-//         if (/^\d+$/.test(input_list.val())) {
-//             list.prepend("<td>" + input_list.val() + "</td>");
-//         }
-//     });
-
-//     $('#right_input').on('click', function () {
-//         if (/^\d+$/.test(input_list.val())) {
-//             list.append("<td>" + input_list.val() + "</td>");
-//         }
-//     });
-
-//     $('#left_out').on('click', function () {
-//         var tmp = list.find('td:first-child').text();
-//         list.find('td:first-child').remove();
-//         alert(tmp);
-//     });
-
-//     $('#right_out').on('click', function () {
-//         var tmp = list.find('td:last-child').text();
-//         list.find('td:last-child').remove();
-//         alert(tmp);
-//     });
-// })
-
 $(function () {
     var arr = [];
     var list = $('#list');
     var input_list = $('#input_list');
+
     list.on("click", "td", function () {  //显示列表，点击删除
         this.remove();
     });
 
     $('#left_input').on('click', function () {  //从左侧输入，
         var strTmp = StrToStrarr(input_list.val())   //测试语法
-        strTmp.map(function(s){
+        strTmp.map(function (s) {
             arr.unshift(s);
         })
-        // arr.unshift(input_list.val());  //压入数组
-        write_list();   //显示
+        write_list(arr);   //显示
         input_list.val('');  //清空输入框
     });
 
     $('#right_input').on('click', function () {
         var strTmp = StrToStrarr(input_list.val());
-        strTmp.map(function(s){
+        strTmp.map(function (s) {
             arr.push(s);
-        }) 
-        write_list();
+        })
+        write_list(arr);
         input_list.val('');
     });
 
     $('#left_out').on('click', function () {   //删除
         arr.shift();
-        write_list();
+        write_list(arr);
     });
 
     $('#right_out').on('click', function () {
         arr.pop();
-        write_list();
+        write_list(arr);
     });
 
-    function write_list() {  //修改dom显示出结果
+    $("#searchButton").on('click', function () {
+        searchStr($('#search').val());
+    })
+    function write_list(arr) {  //修改dom显示出结果
         list.find('td').remove();
         arr.map(function (x) {
-            list.append("<td>" + x + "</td>");
+            list.append("<td class='td_style'>" + x + "</td>");
         })
     }
 
@@ -84,4 +54,18 @@ $(function () {
             }
         });
     }
+
+    function searchStr(ss) {
+        var arrTmp = [];
+        var reg = new RegExp(ss, 'g');
+        write_list(arr);
+        arr.map(function (st) {
+            arrTmp.push(st.replace(reg, '<span class="highlight">' + ss + '</span>'))
+        });
+        write_list(arrTmp);
+    }
+
+    $('#removeAll').on('click', function () {
+        list.find('td').remove();;
+    });
 })
