705c6de0e631ad39a9a379f05e2f0fdbf95e990b
guo <guoyaoquan@wy.net>
2017-02-15 14:58:09 +0800

    commit in 17/2/15

---
+++ task25/app.js
@@ -0,0 +1,104 @@
+$(function () {
+    var divList = [];
+    var timer;
+    var selectToDelete;
+    $('#dlrBtn').on('click', function () {  //前序遍历
+        reset();
+        dlrOrder($('#root'));
+        changeColor();
+    });
+
+    $('#postBtn').on('click', function () {  //后序遍历
+        reset();
+        postOrder($('#root'));
+        changeColor();
+    })
+
+    $('#searchBtn').on('click', function () {   //查找
+        reset();
+        if (searchBinaryTree($('#root'))) {
+            $('#result').text('已找到');
+            changeColor()
+        } else {
+            $('#result').text('找不到')
+        }
+    })
+
+    $('#root').on('click','div', function (e) {   //选中一个节点
+        e.stopPropagation();
+        if (selectToDelete) {
+            selectToDelete.css('background-color', '#fff');
+        }
+        $(this).css('background-color', '#ccc');
+        selectToDelete = $(this);
+    })
+
+    $('#deleteBtn').on('click', function () {   //删除按钮
+        selectToDelete.remove();
+    })
+
+    $('#insertBtn').on('click', function () {
+        selectToDelete.append('<div><span>' + $('#insertNewValue').val() + '</span></div>');
+    })
+
+    /******前序遍历******/
+    function dlrOrder(node) {
+        if (node.html() != null) {
+            divList.push(node);
+            for (var i = 1; i <= node.find('>').length; i++) {
+                dlrOrder(node.find(`>div:nth-child(${i})`));
+            }
+        }
+    }
+
+    /******后序遍历******/
+    function postOrder(node) {
+        if (node.html() != null) {
+            for (var i = 1; i <= node.find('>').length; i++) {
+                postOrder(node.find(`>div:nth-child(${i})`));
+            }
+            divList.push(node);
+        }
+    }
+
+    /*******查找******/
+    function searchBinaryTree(node) {
+        if (node.html() != null) {
+            if ($('#searchValue').val().trim() === node.find('>span').text()) {
+                divList.push(node);
+                return true;
+            }
+            divList.push(node);
+            for (var i = 1; i <= node.find('>').length; i++) {
+                if (searchBinaryTree(node.find(`>div:nth-child(${i})`))) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    /******结果可视化******/
+    function changeColor() {
+        var i = 0;
+        timer = setInterval(function () {
+            if (i > 0) {
+                divList[i - 1].css('background-color', '#fff');
+            }
+            if (i >= divList.length) {
+                clearInterval(timer);
+                divList = [];
+                return;
+            }
+            divList[i].css('background-color', '#ccc');
+            i++;
+        }, 500)
+    }
+
+    /******重置顏色******/
+    function reset() {
+        $('div').css('background-color', '#fff');
+        clearInterval(timer);
+        divList = [];
+    }
+
+})
---
+++ task26/app.js
@@ -0,0 +1,77 @@
+// var ship = $('#ship-fire');
+// var deg = 0;
+// var x = 0;
+// var y = 0;
+// var z = 200;
+// $("#createNewShip").on('click', function () {
+//     setInterval(function () {
+//         ship.css('transform', 'rotate(' + deg * -1 + 'deg)');
+//         y = 75 - z * Math.sin(deg * Math.PI * 2 / 360);
+//         x = 90 + z * Math.cos(deg * Math.PI * 2 / 360);
+//         ship.css('top', y + 'px');
+//         ship.css('left', x + 'px');
+//         deg++;
+//     }, 30)
+// })
+
+var SHIP_TOTAL_ENERGY = 100;  //飞船初始能源
+var SAPCE_SHIP_ARRAY = [];    //飞船队列
+var ORBIT_HEIGHT = 200;       //轨道半径
+var FRAME_PRE_SECOND = 100;   //帧数
+/****飞船类*****/
+var SpaceShip = function (shipId) {
+    this.shipDom = $('#ship-fire');     //飞船DOM
+    this.shipId = shipId;       //飞船号
+    this.status = 'static';     //飞船状态
+    this.totalEnergy = SHIP_TOTAL_ENERGY;    //总能源
+    this.remainEnergy = SHIP_TOTAL_ENERGY;   //剩余能源
+    this.leftPosition = 290;  //left坐标量
+    this.topPosition = 75;   //top坐标量
+    this.deg = 0;      //飞船角坐标角度
+    this.speed = 50;   //飞行角速度，(单位：度/秒)
+    this.ENERGY_CONSUME_PRE_SECOND = 0.05;  //能源消耗速度(每秒百分之几)
+    this.ENERGY_RECOVER_PRE_SECOND=0.02;    //能源回复速度（每秒百分之几）
+}
+
+/*****设置飞船转为飞行状态******/
+SpaceShip.prototype.fly = function () {
+    this.status = 'fly';
+}
+
+/*****设置飞船转为停止状态*******/
+SpaceShip.prototype.stop = function () {
+    this.status = 'stop';
+}
+
+/*****飞船的动画，飞行，充电******/
+SpaceShip.prototype.animation = function () {
+    if (this.status == 'fly') {
+        if (this.remainEnergy <= 0) {
+            this.status = 'stop'
+            return;
+        }
+        this.topPosition = 50 - ORBIT_HEIGHT * Math.sin(this.deg * Math.PI * 2 / 360);    //计算top和left的值
+        this.leftPosition = 85 + ORBIT_HEIGHT * Math.cos(this.deg * Math.PI * 2 / 360);
+        this.deg += this.speed / FRAME_PRE_SECOND;          //计算每帧改变多少角度
+        this.remainEnergy -= this.totalEnergy * this.ENERGY_CONSUME_PRE_SECOND / FRAME_PRE_SECOND;    //计算每帧减少多少能源
+        this.shipDom.css('top', this.topPosition + 'px');   //设css
+        this.shipDom.css('left', this.leftPosition + 'px');
+        this.shipDom.css('transform', 'rotate(' + this.deg * -1 + 'deg)');
+        this.shipDom.find('.remainEnergy').text(Math.round(this.remainEnergy)+"%");
+    }
+    if (this.status == 'stop') {
+        this.remainEnergy += this.totalEnergy * this.ENERGY_RECOVER_PRE_SECOND / FRAME_PRE_SECOND;    //计算每帧回复多少能源
+        this.shipDom.find('.remainEnergy').text(Math.round(this.remainEnergy)+"%");
+    }
+}
+
+
+
+var ship1 = new SpaceShip(1);
+ship1.fly();
+
+
+setInterval(function () {
+    ship1.animation();
+
+}, 1000 / FRAME_PRE_SECOND)
