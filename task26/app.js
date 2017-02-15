d7ca60e13ceed31835b3ed7828b02eebcc60256b
guo <guoyaoquan@wy.net>
2017-02-15 18:44:59 +0800

    add mediator

--- task26/app.js
+++ task26/app.js
@@ -1,35 +1,23 @@
-// var ship = $('#ship-fire');
-// var deg = 0;
-// var x = 0;
-// var y = 0;
-// var z = 200;
-// $("#createNewShip").on('click', function () {
-//     setInterval(function () {
-//         ship.css('transform', 'rotate(' + deg * -1 + 'deg)');
-//         y = 75 - z * Math.sin(deg * Math.PI * 2 / 360);
-//         x = 90 + z * Math.cos(deg * Math.PI * 2 / 360);
-//         ship.css('top', y + 'px');
-//         ship.css('left', x + 'px');
-//         deg++;
-//     }, 30)
-// })
-
 var SHIP_TOTAL_ENERGY = 100;  //飞船初始能源
 var SAPCE_SHIP_ARRAY = [];    //飞船队列
 var ORBIT_HEIGHT = 200;       //轨道半径
 var FRAME_PRE_SECOND = 100;   //帧数
+var MAX_SHIP_NUM = 4;         //最大飞船数
+
 /****飞船类*****/
 var SpaceShip = function (shipId) {
     this.shipDom = $('#ship-fire');     //飞船DOM
+
     this.shipDom = (function () {
         $('.plant').append('<div class="ship-fire" id="spaceShip-' + shipId + '">' +
             '<div class="ship">' +
-            '<div class="shipId">1号</div>' +
+            '<div class="shipId">' + shipId + '号</div>' +
             '<div class="remainEnergy">100%</div>' +
             '</div>' +
             '</div>')
         return $('#spaceShip-' + shipId);
     })()
+
     this.shipId = shipId;       //飞船号
     this.status = 'static';     //飞船状态
     this.totalEnergy = SHIP_TOTAL_ENERGY;    //总能源
@@ -78,48 +66,109 @@ SpaceShip.prototype.animation = function () {
 
 SpaceShip.prototype.radio = function (signal) {    //电台
     if (this.shipId == signal.id) {
-        this[signal.commond];
+        this[signal.commond]();
+        // this.fly();
     }
 }
 
-SpaceShip.prototype.destructiveMechanism = function () {   //自爆装置
+SpaceShip.prototype.destructive = function () {   //自爆装置
     this.shipDom.remove();
 }
 
+
 var Commander = function (commanderId) {    //指挥官类
     this.commanderId = commanderId;
-    this.commanderDom = (function () {
-        $('#commander-area').append('<div class="control-area" id="commander-' + commanderId + '" >' +
-            '<input id="createNewShip" value="飞船飞行" type="button" />' +
-            '</div>')
-        return $('#commander-' + commanderId);
-    })()
+    this.commanderDom = null;
+    this.spaceShipArray = [];
 }
 
-Commander.prototype.orderCreateNewShip = function (shipId) {   //起飞新的飞船的命令
-    var ship = new SpaceShip(1);
+Commander.prototype.init = function () {    //初始化指挥官类
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {    //初始化数组
+        this.spaceShipArray[i] = false;
+    }
+    $('#commander-area').append('<div class="control-area" id="commander-' + this.commanderId + '" >' +   //添加dom节点
+        '<input class="createNewShipBtn" value="起飞新的飞船" type="button" onclick="commander' + this.commanderId + '.orderCreateNewShip()"/>' +
+        '</div>');
+    this.commanderDom = $('#commander-' + this.commanderId);
+}
+
+Commander.prototype.getEmptyId = function () {    //获得可用id
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+        if (!this.spaceShipArray[i]) {
+            return i;
+        } else if (i >= MAX_SHIP_NUM) {
+            return false;
+        }
+    }
+}
+
+Commander.prototype.getShipArray = function () {     //返回该指挥官的飞船组
+    return this.spaceShipArray;
+}
+
+Commander.prototype.orderCreateNewShip = function () {   //起飞新的飞船的命令
+    var emptyId = this.getEmptyId();
+    if (emptyId == false) {
+        console.log('飞船数到达上限');
+        return;
+    }
+    var ship = new SpaceShip(emptyId);    //new飞船实例
+    this.spaceShipArray[emptyId] = ship;
+    this.commanderDom.append('<div id="shipControl-' + emptyId + '"><span>' + emptyId + '号飞船</span>' +
+        '<input type="button" value="开始飞行" onclick="commander' + this.commanderId + '.orderBeginFly(' + emptyId + ')"></input>' +
+        '<input type="button" value="停止飞行" onclick="commander' + this.commanderId + '.orderStopFly(' + emptyId + ')"></input>' +
+        '<input type="button" value="摧毁飞船" onclick="commander' + this.commanderId + '.orderDestory(' + emptyId + ')"></input></div>');
 }
 
 Commander.prototype.orderBeginFly = function (shipId) {    //命令：开始飞行
-
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+        if (this.spaceShipArray[i]) {
+            this.spaceShipArray[i].radio({ id: shipId, commond: 'fly' });
+        }
+    }
 }
 
 Commander.prototype.orderStopFly = function (shipId) {     //命令：停止飞行
-
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+        if (this.spaceShipArray[i]) {
+            this.spaceShipArray[i].radio({ id: shipId, commond: 'stop' });
+        }
+    }
 }
 
 Commander.prototype.orderDestory = function (shipId) {     //命令：摧毁飞船
-
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+        if (this.spaceShipArray[i]) {
+            this.spaceShipArray[i].radio({ id: shipId, commond: 'destructive' });
+        }
+    }
+    $('#shipControl-' + shipId).remove();
+    this.spaceShipArray[shipId] = false;
 }
 
-// Commander.prototype.dom = function(){
-//     console.log(this.commanderDom.html());
-// }
+var Mediator = (function (cmd) {
+    this.cmd = cmd;
+    this.sendBroadcase = function () {
+        var spaceShipArray = commander1.getShipArray();
+        for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+            if (spaceShipArray[i]) {
+                spaceShipArray[i].radio({ id: shipId, commond: 'fly' });
+            }
+        }
+    }
+})()
 
-var commander1 = new Commander(1);
-commander1.orderCreateNewShip(1);
-// commander1.dom();
-// setInterval(function () {
-//     ship1.animation();
 
-// }, 1000 / FRAME_PRE_SECOND)
+
+
+var commander1 = new Commander(1);      //建立指挥官实例
+commander1.init(1);                     //初始化
+
+setInterval(function () {
+    var spaceShipArray = commander1.getShipArray();
+    for (var i = 1; i <= MAX_SHIP_NUM; i++) {
+        if (spaceShipArray[i]) {
+            spaceShipArray[i].animation();
+        }
+    }
+}, 1000 / FRAME_PRE_SECOND)
